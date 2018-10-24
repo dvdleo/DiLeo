19262b9ad4eae2ce8c99698eab1921b122252c5e
diilek <atas.dilek9@gmail.com>
2018-10-24 12:11:02 +0200

    archi

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-salut
-coucou ça va ? 
+<?php>
+$title ='Accueil';
+$description = '';
+?>
 
-ça va et toi?
+    <?php include('layouts/header.php'); ?> 
+    <main>
+        <div class="main">
+        
+            <div class="row">
+                <div class="col-sm-12 col-lg-8">
+        // MON CONTENU PRINCIPALE
+                </div>
+            </div>
+            <div class="col-sm-12 col-lg-4">
+        // MA SIDE BAR
+            </div>
+        </div>
+    </main>
+     <?php include('layouts/footer.php'); ?>
+   
