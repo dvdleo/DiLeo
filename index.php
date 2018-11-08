35f6f09c7e3fbb68902c5dffea320d453b12ee01
diilek <atas.dilek9@gmail.com>
2018-11-08 09:16:36 +0100

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -7,11 +7,32 @@ $description = '';
 <?php include('layouts/header.php'); ?>
 
 <div class="row">
-	<div class="col-sm-12 col-lg-8">
-		<h1>Les prochains évènements</h1>
-		<div>
-		</div>
-	</div>
+    <div class="col-sm-12 col-lg-8">
+        <h1>Les prochains évènements</h1>
+
+        <?php require_once('php/get_events.php'); ?>
+        
+        <ul>
+            <?php foreach($events as $event){ ?> 
+            <?php if($event['category_id'] == 1){ ?>
+            <li><?php echo $event['name']; ?></li>
+            <?php } ?>
+            <?php } ?>
+        
+        </ul>
+        
+        <br>
+        
+        <ul>
+            <?php foreach($events as $event){ ?> 
+            <?php if($event['category_id'] == 2){ ?>
+            <li><?php echo $event['name']; ?></li>
+            <?php } ?>
+            <?php } ?>
+        
+        </ul>
+
+    </div>
 </div>
 
 
